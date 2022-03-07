73bf38e0d0253b4b2dc3e36f6a84ff130c9ae2f8
Giulia Menghini <94474813+giuliamenghini1@users.noreply.github.com>
2022-03-07 12:57:33 -0300

    Update Query Lucro Bruto Carteira Hunter.sql

--- Query Lucro Bruto Carteira Hunter.sql
+++ Query Lucro Bruto Carteira Hunter.sql
@@ -1,7 +1,8 @@
 SELECT
     *
-FROM `ctra-comercial-1554819299431.metabase_planejamento_central.analitico_lucro_bruto`
+FROM `ctra-comercial-1554819299431.metabase_planejamento_central.analitico_lucro_bruto_2021_12`
 WHERE 1=1
     AND Subcanal = 'FRANQUIA'
     AND Forca_de_Venda = 'FRANQUIA'
-    AND Data_de_Cadastro BETWEEN '2022-01-01' AND '2022-01-31'
+    AND Mes = '2021-12-31'
+    --AND LAST_DAY(Data_de_Ativacao) = '2021-12-31'
