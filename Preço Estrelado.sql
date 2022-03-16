47ba472bcb52273cd5a16e35e8e008cf7206668c
Giulia Menghini <94474813+giuliamenghini1@users.noreply.github.com>
2022-03-16 14:53:18 -0300

    Update Preço Estrelado.sql

--- Preço Estrelado.sql
+++ Preço Estrelado.sql
@@ -1,3 +1,24 @@
-SELECT * FROM `ctra-comercial-1554819299431.core_apurations.generated_vw_clients_portfolio_franchise_2022_01_31`
-WHERE str_affiliator_vendor_sales_force_name = 'FRANQUIA'
-AND str_sub_channel	= 'FRANQUIA'
+SELECT
+    Stonecode,
+    Cliente,
+    DATE(EXTRACT(YEAR FROM Data_de_Credenciamento), EXTRACT(MONTH FROM Data_de_Credenciamento), EXTRACT(DAY FROM Data_de_Credenciamento)) as Data_de_Credenciamento,
+    Email_do_Agente,
+    Estrelas,
+    Regional,
+    Distrito,
+    Polo
+FROM `ctra-comercial-1554819299431.funil_de_vendas.novos_clientes`
+WHERE 1=1
+    AND LAST_DAY(DATE(EXTRACT(YEAR FROM Data_de_Credenciamento), EXTRACT(MONTH FROM Data_de_Credenciamento), EXTRACT(DAY FROM Data_de_Credenciamento))) = '2022-02-28'
+    AND Subcanal = 'FRANQUIA'
+    AND Perfil = 'Comercial - Franquia Polos'
+
+GROUP BY   
+    Stonecode,
+    Cliente,
+    Data_de_Credenciamento,
+    Email_do_Agente,
+    Estrelas,
+    Regional,
+    Distrito,
+    Polo
